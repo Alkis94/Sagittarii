dab8e14ceb165f8da0833cf9566bfc3e4d3fc599
DESKTOP-712T0JH\Alkis <alkiviadis1994@hotmail.com>
2019-03-02 15:41:13 +0200

    Artwork for menus/button/texts etc. Other minor improvements.

---
+++ Assets/Scripts/Menus/Buttons/Cross.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cross : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject thisMenu;
+
+    public void OnCrossClick ()
+    {
+        thisMenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Shop/ShopOrb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopOrb : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject orbMenu;
+
+    void Start()
+    {
+        orbMenu.SetActive(false);
+    }
+
+
+    public void OnOrbClick()
+    {
+        orbMenu.SetActive(true);
+    }
+
+
+}
