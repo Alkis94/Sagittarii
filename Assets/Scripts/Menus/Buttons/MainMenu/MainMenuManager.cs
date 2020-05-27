09e5c302112e937c3cf94aaff83279a9f20dcbcb
Alkis94 <alkiviadis1994@hotmail.cm>
2020-05-27 19:43:08 +0300

    Started reworking audio. Started reworking button/menu scripts!

---
+++ Assets/Scripts/Menus/Buttons/MainMenu/MainMenuManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject quitMenu;
+    [SerializeField]
+    private Transform mainButtons;
+
+    private void Start()
+    {
+        quitMenu.SetActive(false);
+    }
+
+    public void OnPlayPress()
+    {
+        SceneManager.LoadScene("Town");
+        GameState.UnpauseGame();
+    }
+
+    public void OnQuitPress()
+    {
+        quitMenu.SetActive(true);
+        foreach (Transform button in mainButtons)
+        {
+            button.GetComponent<Button>().enabled = false;
+        }
+    }
+
+    public void OnQuitYesPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnQuitNoPress()
+    {
+        quitMenu.SetActive(false);
+        foreach (Transform button in mainButtons)
+        {
+            button.GetComponent<Button>().enabled = true;
+        }
+    }
+
+}
