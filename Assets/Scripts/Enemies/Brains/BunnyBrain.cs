42b31fab7511e72f19c8fb942cc57c748b2a5638
Alkis94 <32446787+Alkis94@users.noreply.github.com>
2020-11-23 19:06:43 +0200

    Added small bunny. Finished deer.

--- Assets/Scripts/Enemies/Brains/BunnyBrain.cs
+++ Assets/Scripts/Enemies/Brains/BunnyBrain.cs
@@ -2,6 +2,10 @@
 
 public class BunnyBrain : GroundEnemyBrain
 {
+    [SerializeField]
+    private float rayOriginOffset = 0;
+
+
     protected override void Awake()
     {
         base.Awake();
@@ -36,7 +40,7 @@ public class BunnyBrain : GroundEnemyBrain
 
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walking"))
         {
-            Vector3 origin = new Vector3(transform.position.x, transform.position.y - 1, 0);
+            Vector3 origin = new Vector3(transform.position.x, transform.position.y + rayOriginOffset, 0);
             RaycastHit2D hit = Physics2D.Raycast(origin, transform.right, 10, 1 << LayerMask.NameToLayer("Player"));
             if (hit)
             {
