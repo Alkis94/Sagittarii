f984b0b678f87d95c82f8073950d571bec3e30ec
Alkis94 <32446787+Alkis94@users.noreply.github.com>
2020-11-24 14:48:41 +0200

    Changed how to play to Hotkeys / Tips.

---
+++ Assets/Scripts/Menus/HowToPlay.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HowToPlay : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> tips;
+    private int index;
+
+    public void LeftArrow ()
+    {
+
+        tips[index].SetActive(false);
+
+        if(index == 0)
+        {
+            index = tips.Count - 1;
+        }
+        else
+        {
+            index--;
+        }
+
+        tips[index].SetActive(true);
+    }
+
+    public void RightArrow()
+    {
+        tips[index].SetActive(false);
+
+        if (index == tips.Count - 1)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+
+        tips[index].SetActive(true);
+    }
+
+}
